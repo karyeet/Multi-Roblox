0064f11d70b72bd7f736026fc42e5d42c8451547
Kareem <28524112+karyeet@users.noreply.github.com>
2022-01-27 15:25:34 -0800

    ive grown

--- multiroblox.cpp
+++ multiroblox.cpp
@@ -1,14 +1,12 @@
 #include <Windows.h>
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main()
 {
 	CreateMutex(NULL, TRUE, "ROBLOX_singletonMutex");
 
-	cout << "You can now open multiple ROBLOX clients." << endl;
-    	cout << "Closing this window will close all but one client." << endl << endl;
+	printf("You can now open multiple ROBLOX clients.\n");
+    	printf("Closing this window will close all but one client.\n\n");
 	
 	system("pause");
 	return 0;
